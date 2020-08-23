c3073e6b051be5a4a603176f91a1966c8e6d7ed3
Marcin <33165389+Marshall00@users.noreply.github.com>
2020-08-23 22:25:07 +0200

    Add files via upload

---
+++ SimpleChattyBot_Project/bot.py
@@ -0,0 +1,60 @@
+def greet(bot_name, birth_year):
+    print('Hello! My name is ' + bot_name + '.')
+    print('I was created in ' + birth_year + '.')
+
+
+def remind_name():
+    print('Please, remind me your name.')
+    name = input()
+    print('What a great name you have, ' + name + '!')
+
+
+def guess_age():
+    print('Let me guess your age.')
+    print('Enter remainders of dividing your age by 3, 5 and 7.')
+
+    rem3 = int(input())
+    rem5 = int(input())
+    rem7 = int(input())
+    age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105
+
+    print("Your age is " + str(age) + "; that's a good time to start programming!")
+
+
+def count():
+    print('Now I will prove to you that I can count to any number you want.')
+
+    num = int(input())
+    curr = 0
+    while curr <= num:
+        print(curr, '!')
+        curr = curr + 1
+
+
+def test():
+    print("Let's test your programming knowledge.")
+    print('What is part of construction declaration in Python?')
+    print('1. __def__')
+    print('2. __start__')
+    print('3. __begin__')
+    print('4. __constr__')
+
+    user_ans = int(input())
+
+    while user_ans != 1:
+        print('Please, try again')
+        user_ans = int(input())
+    else:
+        print('Completed, have a nice day!')
+
+
+def end():
+    print('Congratulations, have a nice day!')
+
+
+greet('Aid', '2020')  # change it as you need
+remind_name()
+guess_age()
+count()
+test()
+end()
